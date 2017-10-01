af15e6e958745043173b9a8f1ddacc50ec0c53f3
Alloyer <alexeygruntov@yandex.ru>
2017-10-01 19:08:34 +0300

    Added initializing and showing one field

--- SeaBattle_CPP/SeaBattle_CPP.cpp
+++ SeaBattle_CPP/SeaBattle_CPP.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include "fstream"
+#include "Map.h"
+#include <string>
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
+
+	std::string FileName;
+	std::cout << "¬ведите название файла с картой\n";
+	std::cin >> FileName;
+	Map *map = new Map();
+	map->initialize_field(FileName);
+
     return 0;
 }
 
