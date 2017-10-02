d564e2372bbde42f543a668dc899d05ee0e979fe
Alloyer <alexeygruntov@yandex.ru>
2017-10-02 22:32:16 +0300

    Добвалены точечные проверки постановки кораблей компьютером для горизонтального положения.

--- SeaBattle_CPP/AI.h
+++ SeaBattle_CPP/AI.h
@@ -26,16 +26,14 @@ public:
 		srand(time(0));
 		for (int i = 0; i < 10; i++)
 		{
-			srand(time(0));
 			int x = rand() % 9;
 			int y = rand() % 9;
 			int orientation = (rand() % 9) % 2;
 			while (!isOK(x, y, Ships[i], orientation))
 			{
-				srand(time(0));
-				int x = rand() % 9;
-				int y = rand() % 9;
-				int orientation = (rand() % 9) % 2;
+				x = rand() % 9;
+				y = rand() % 9;
+				orientation = (rand() % 9) % 2;
 			}
 			setShip(x, y, Ships[i], orientation);
 		}
@@ -53,7 +51,130 @@ private:
 	// ориентация = 0 - горизонтально; 1 - вертикально.
 	bool isOK(const int& x, const int& y, const int& size, const int& orientation) const
 	{
-		if (orientation == 0)
+		if (orientation == 0 || size == 1)
+		{
+			if (y >= 0 && y <= 10 - size && x >= 0 && x <= 9)
+			{
+
+				//случай с (0,0)
+				if (y == 0 && x == 0) 
+				{
+					for (int i = 0; i <= size; i++)
+					{
+						if (Field[x + 1][y + i] != 'O') return false;
+					}
+					if (Field[x][y + size] != 'O') return false;
+					return true;
+				}
+
+				//случай с (0,10-size)
+				if (y == 10 - size && x == 0) 
+				{
+					for (int i = -1; i < size; i++)
+					{
+						if (Field[x + 1][y + i] != 'O') return false;
+					}
+					if (Field[x][y - 1] != 'O') return false;
+					return true;
+				}
+
+				//случай с (0, 1-8[size])
+				if (y >= 1 && y < 10 - size && x == 0)
+				{
+					if (Field[x][y + size] != 'O') return false;
+					if (Field[x][y - 1] != 'O') return false;
+					for (int i = -1; i <= size; i++)
+					{
+						if (Field[x + 1][y + i] != 'O') return false;
+					}
+					return true;
+				}
+
+				//случай с (9,0)
+				if (y == 0 && x == 9)
+				{
+					for (int i = 0; i <= size; i++)
+					{
+						if (Field[x - 1][y + i] != 'O') return false;
+					}
+					if (Field[x][y + size] != 'O') return false;
+					return true;
+				}
+
+				//случай с (9,10-size)
+				if (y == 10 - size && x == 9)
+				{
+					for (int i = -1; i < size; i++)
+					{
+						if (Field[x - 1][y + i] != 'O') return false;
+					}
+					if (Field[x][y - 1] != 'O') return false;
+					return true;
+				}
+
+				//случай с (9,1-8[size])
+				if (y >= 1 && y < 10 - size && x == 9)
+				{
+					if (Field[x][y + size] != 'O') return false;
+					if (Field[x][y - 1] != 'O') return false;
+					for (int i = -1; i <= size; i++)
+					{
+						if (Field[x - 1][y + i] != 'O') return false;
+					}
+					return true;
+				}
+
+				//случай с (1-8,0)
+				if (y == 0 && x >= 1 && x <= 8)
+				{
+					for (int i = -1; i <= 1; i += 2)
+					{
+						for (int j = 0; j <= size; j++)
+						{
+							if (Field[x + i][y + j] != 'O') return false;
+						}
+					}
+					if (Field[x][y + size] != 'O') return false;
+					return true;
+				}
+
+				//случай с (1-8,10-size)
+				if (y == 10 - size && x >= 1 && x <= 8)
+				{
+					for (int i = -1; i <= 1; i += 2)
+					{
+						for (int j = -1; j < size; j++)
+						{
+							if (Field[x + i][y + j] != 'O') return false;
+						}
+					}
+					if (Field[x][y - 1] != 'O') return false;
+					return true;
+				}
+				
+				//случаи с (1-8, 1-8)
+				if (y >= 1 && y <= 8 && x >= 1 && x <= 8)
+				{
+					for (int i = -1; i <= 1; i++)
+					{
+						for (int j = -1; j <= size; j++)
+						{
+							if (Field[x + i][y + j] != 'O') return false;
+						}
+					}
+					return true;
+				}
+				
+				////случаи с (1-8, 1-8)
+				//for (int i = 0; i < size; i++)
+				//{
+				//	if (Field[x][y + i] != 'O') return false;
+				//}
+				//return true;
+			}
+			else return false;
+		}
+		else if(orientation == 1)//TODO: сделать точечные проверки для  вертикальной ориентации
 		{
 			if (y >= 0 && y <= 9 && x >= 0 && x <= 10 - size)
 			{
@@ -65,36 +186,25 @@ private:
 			}
 			else return false;
 		}
-		else if(orientation == 1)
-		{
-			if (x >= 0 && x <= 9 && y >= 0 && y <= 10 - size)
-			{
-				for (int i = 0; i < size; i++)
-				{
-					if (Field[x][y + i] != 'O') return false;
-				}
-				return true;
-			}
-			else return false;
-		}
 	}
 
 	void setShip(const int& x, const int& y, const int& size, const int& orientation)
 	{
-		if (orientation == 0)
-		{
-			for (int i = 1; i <= size; i++)
-			{
-				Field[x + i - 1][y] = 'S';
-			}
-		}
-		else if(orientation == 1)
+		if (orientation == 0 || size == 1)
 		{
 			for (int i = 1; i <= size; i++)
 			{
 				Field[x][y + i - 1] = 'S';
 			}
 		}
+		else if(orientation == 1)
+		{
+			for (int i = 1; i <= size; i++)
+			{
+				Field[x + i - 1][y] = 'S';
+			}
+		}
+		
 	}
 
 	char Field[10][10];
