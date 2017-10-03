2dda5a2a81fe9dc27f28b0d9b3dc0ab8ee2717a7
Alloyer <alexeygruntov@yandex.ru>
2017-10-03 14:42:47 +0300

    Расстановка кораблей компьютера

--- SeaBattle_CPP/AI.h
+++ SeaBattle_CPP/AI.h
@@ -48,126 +48,171 @@ private:
 	// ориентация = 0 - горизонтально; 1 - вертикально.
 	bool isOK(const int& x, const int& y, const int& size, const int& orientation) const
 	{
-		if (orientation == 0 || size == 1)
+		if (orientation == 0)
 		{
 			if (y >= 0 && y <= 10 - size && x >= 0 && x <= 9)
 			{
 
-				//случай с (0,0)
-				if (y == 0 && x == 0) 
+				for (int i = -1; i <= 1; i++)
 				{
-					for (int i = 0; i <= size; i++)
+					if (x + i < 0 || x + i > 9) continue;
+					for (int j = -1; j <= size; j++)
 					{
-						if (Field[x + 1][y + i] != 'O') return false;
+						if (y + j < 0 || y + j > 9) continue;
+						if (Field[x + i][y + j] != 'O') return false;
 					}
-					if (Field[x][y + size] != 'O') return false;
-					return true;
 				}
-
-				//случай с (0,10-size)
-				if (y == 10 - size && x == 0) 
-				{
-					for (int i = -1; i < size; i++)
-					{
-						if (Field[x + 1][y + i] != 'O') return false;
-					}
-					if (Field[x][y - 1] != 'O') return false;
-					return true;
-				}
-
-				//случай с (0, 1-8[size])
-				if (y >= 1 && y < 10 - size && x == 0)
-				{
-					if (Field[x][y + size] != 'O') return false;
-					if (Field[x][y - 1] != 'O') return false;
-					for (int i = -1; i <= size; i++)
-					{
-						if (Field[x + 1][y + i] != 'O') return false;
-					}
-					return true;
-				}
-
-				//случай с (9,0)
-				if (y == 0 && x == 9)
-				{
-					for (int i = 0; i <= size; i++)
-					{
-						if (Field[x - 1][y + i] != 'O') return false;
-					}
-					if (Field[x][y + size] != 'O') return false;
-					return true;
-				}
-
-				//случай с (9,10-size)
-				if (y == 10 - size && x == 9)
-				{
-					for (int i = -1; i < size; i++)
-					{
-						if (Field[x - 1][y + i] != 'O') return false;
-					}
-					if (Field[x][y - 1] != 'O') return false;
-					return true;
-				}
-
-				//случай с (9,1-8[size])
-				if (y >= 1 && y < 10 - size && x == 9)
-				{
-					if (Field[x][y + size] != 'O') return false;
-					if (Field[x][y - 1] != 'O') return false;
-					for (int i = -1; i <= size; i++)
-					{
-						if (Field[x - 1][y + i] != 'O') return false;
-					}
-					return true;
-				}
-
-				//случай с (1-8,0)
-				if (y == 0 && x >= 1 && x <= 8)
-				{
-					for (int i = -1; i <= 1; i += 2)
-					{
-						for (int j = 0; j <= size; j++)
-						{
-							if (Field[x + i][y + j] != 'O') return false;
-						}
-					}
-					if (Field[x][y + size] != 'O') return false;
-					return true;
-				}
-
-				//случай с (1-8,10-size)
-				if (y == 10 - size && x >= 1 && x <= 8)
-				{
-					for (int i = -1; i <= 1; i += 2)
-					{
-						for (int j = -1; j < size; j++)
-						{
-							if (Field[x + i][y + j] != 'O') return false;
-						}
-					}
-					if (Field[x][y - 1] != 'O') return false;
-					return true;
-				}
-				
-				//случаи с (1-8, 1-8)
-				if (y >= 1 && y <= 8 && x >= 1 && x <= 8)
-				{
-					for (int i = -1; i <= 1; i++)
-					{
-						for (int j = -1; j <= size; j++)
-						{
-							if (Field[x + i][y + j] != 'O') return false;
-						}
-					}
-					return true;
-				}
-				
-				////случаи с (1-8, 1-8)
-				//for (int i = 0; i < size; i++)
+				return true;
+				////случай с (0,0)
+				//if (y == 0 && x == 0) 
 				//{
-				//	if (Field[x][y + i] != 'O') return false;
+				//	for (int i = 0; i <= 1; i++)
+				//	{
+				//		for (int j = 0; j <= 1; j++)
+				//		{
+				//			if (Field[x + i][y + j] != 'O') return false;
+				//		}
+				//	}
+				//	/*for (int i = 0; i <= size; i++)
+				//	{
+				//		if (Field[x + 1][y + i] != 'O') return false;
+				//	}
+				//	if (Field[x][y + size] != 'O') return false;*/
+				//	return true;
 				//}
-				//return true;
+				//
+				////случай с (0,10-size)
+				//if (y == 10 - size && x == 0) 
+				//{
+				//	for (int i = 0; i <= 1; i++)
+				//	{
+				//		for (int j = -1; j < size; j++)
+				//		{
+				//			if (Field[x + i][y + j] != 'O') return false;
+				//		}
+				//	}
+				//	/*for (int i = -1; i < size; i++)
+				//	{
+				//		if (Field[x + 1][y + i] != 'O') return false;
+				//	}
+				//	if (Field[x][y - 1] != 'O') return false;*/
+				//	return true;
+				//}
+				//
+				////случай с (0, 1-8[size])
+				//if (y >= 1 && y < 10 - size && x == 0)
+				//{
+				//	for (int i = 0; i <= 1; i++)
+				//	{
+				//		for (int j = -1; j <= size; j++)
+				//		{
+				//			if (Field[x + i][y + j] != 'O') return false;
+				//		}
+				//	}
+				//	/*if (Field[x][y + size] != 'O') return false;
+				//	if (Field[x][y - 1] != 'O') return false;
+				//	for (int i = -1; i <= size; i++)
+				//	{
+				//		if (Field[x + 1][y + i] != 'O') return false;
+				//	}*/
+				//	return true;
+				//}
+				//
+				////случай с (9,0)
+				//if (y == 0 && x == 9)
+				//{
+				//	for (int i = -1; i <= 0; i++)
+				//	{
+				//		for (int j = 0; j <= 1; j++)
+				//		{
+				//			if (Field[x + i][y + j] != 'O') return false;
+				//		}
+				//	}
+				//	/*for (int i = 0; i <= size; i++)
+				//	{
+				//		if (Field[x - 1][y + i] != 'O') return false;
+				//	}
+				//	if (Field[x][y + size] != 'O') return false;*/
+				//	return true;
+				//}
+				//
+				////случай с (9,10-size)
+				//if (y == 10 - size && x == 9)
+				//{
+				//	for (int i = -1; i <= 0; i++)
+				//	{
+				//		for (int j = -1; j < size; j++)
+				//		{
+				//			if (Field[x + i][y + j] != 'O') return false;
+				//		}
+				//	}
+				//	/*for (int i = -1; i < size; i++)
+				//	{
+				//		if (Field[x - 1][y + i] != 'O') return false;
+				//	}
+				//	if (Field[x][y - 1] != 'O') return false;*/
+				//	return true;
+				//}
+				//
+				////случай с (9,1-8[size])
+				//if (y >= 1 && y < 10 - size && x == 9)
+				//{
+				//	if (Field[x][y + size] != 'O') return false;
+				//	if (Field[x][y - 1] != 'O') return false;
+				//	for (int i = -1; i <= size; i++)
+				//	{
+				//		if (Field[x - 1][y + i] != 'O') return false;
+				//	}
+				//	return true;
+				//}
+				//
+				////случай с (1-8,0)
+				//if (y == 0 && x >= 1 && x <= 8)
+				//{
+				//	for (int i = -1; i <= 1; i += 2)
+				//	{
+				//		for (int j = 0; j <= size; j++)
+				//		{
+				//			if (Field[x + i][y + j] != 'O') return false;
+				//		}
+				//	}
+				//	if (Field[x][y + size] != 'O') return false;
+				//	return true;
+				//}
+				//
+				////случай с (1-8,10-size)
+				//if (y == 10 - size && x >= 1 && x <= 8)
+				//{
+				//	for (int i = -1; i <= 1; i += 2)
+				//	{
+				//		for (int j = -1; j < size; j++)
+				//		{
+				//			if (Field[x + i][y + j] != 'O') return false;
+				//		}
+				//	}
+				//	if (Field[x][y - 1] != 'O') return false;
+				//	return true;
+				//}
+				//
+				////случаи с (1-8, 1-8)
+				//if (y >= 1 && y <= 8 && x >= 1 && x <= 8)
+				//{
+				//	for (int i = -1; i <= 1; i++)
+				//	{
+				//		for (int j = -1; j <= size; j++)
+				//		{
+				//			if (Field[x + i][y + j] != 'O') return false;
+				//		}
+				//	}
+				//	return true;
+				//}
+				//
+				//////случаи с (1-8, 1-8)
+				////for (int i = 0; i < size; i++)
+				////{
+				////	if (Field[x][y + i] != 'O') return false;
+				////}
+				////return true;
 			}
 			else return false;
 		}
@@ -176,48 +221,90 @@ private:
 			if (y >= 0 && y <= 9 && x >= 0 && x <= 10 - size)
 			{
 
-				//случай с (0,0)
-				//
-
-				//случай с (0,9)
-				//
-
-				//случай с (10-size,0)
-				//
-
-				//случай с (10-size,9)
-				//
-
-				//случай с (0,1-8)
-				//
-
-				//случай с (10-size,1-8)
-				//
-
-				//случай с (1-8,0)
-				//
-
-				//случай с (1-8,9)
-				//
-
-				//случаи с (1-8,1-8)
-				if (y >= 1 && y <= 8 && x >= 1 && x <= 8)
+				for (int i = -1; i <= size; i++)
 				{
-					for (int i = -1; i <= 1; i++)
+					if (x + i < 0 || x + i > 9) continue;
+					for (int j = -1; j <= 1; j++)
 					{
-						for (int j = -1; j <= size; j++)
-						{
-							if (Field[x + j][y + i] != 'O') return false;
-						}
+						if (y + j < 0 || y + j > 9) continue;
+						if (Field[x + i][y + j] != 'O') return false;
 					}
-					return true;
 				}
-
-				/*for (int i = 0; i < size; i++)
-				{
-					if (Field[x + i][y] != 'O') return false;
-				}
-				return true;*/
+				return true;
+				////случай с (0,0)
+				//if (x == 0 && y == 0)
+				//{
+				//	for (int i = 0; i <= size; i++)
+				//	{
+				//		if (Field[x + i][y + 1] != 'O') return false;
+				//	}
+				//	if (Field[x + size][y] != 'O') return false;
+				//	return true;
+				//}
+				////
+				////случай с (0,9)
+				//if (x == 0 & y == 9)
+				//{
+				//	for (int i = 0; i <= size; i++)
+				//	{
+				//		if (Field[x + i][y - 1] != 'O') return false;
+				//	}
+				//	if (Field[x + size][y] != 'O') return false;
+				//	return true;
+				//}
+				////
+				////случай с (10-size,0)
+				//if (x == 10 - size && y == 0)
+				//{
+				//	for (int i = -1; i < size; i++)
+				//	{
+				//		if (Field[x + i][y + 1] != 'O') return false;
+				//	}
+				//	if (Field[x - 1][y] != 'O') return false;
+				//	return true;
+				//}
+				////
+				////случай с (10-size,9)
+				//if (x == 10 - size && y == 9)
+				//{
+				//	for (int i = -1; i < size; i++)
+				//	{
+				//		if (Field[x + i][y - 1] != 'O') return false;
+				//	}
+				//	if (Field[x - 1][y] != 'O') return false;
+				//	return true;
+				//}
+				////
+				////случай с (0,1-8)
+				////
+				////
+				////случай с (10-size,1-8)
+				////
+				////
+				////случай с (1-8[size],0)
+				////
+				////
+				////случай с (1-8[size],9)
+				////
+				////
+				////случаи с (1-8,1-8)
+				//if (y >= 1 && y <= 8 && x >= 1 && x <= 8)
+				//{
+				//	for (int i = -1; i <= 1; i++)
+				//	{
+				//		for (int j = -1; j <= size; j++)
+				//		{
+				//			if (Field[x + j][y + i] != 'O') return false;
+				//		}
+				//	}
+				//	return true;
+				//}
+				//
+				///*for (int i = 0; i < size; i++)
+				//{
+				//	if (Field[x + i][y] != 'O') return false;
+				//}
+				//return true;*/
 			}
 			else return false;
 		}
